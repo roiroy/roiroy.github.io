f364480c36bb2711b3a9b0ca04cd8110dd7c9f02
itrend <itrend@gmail.com>
2019-05-12 19:11:06 +0300

    Initial

---
+++ recalc/precache-manifest.19c7efd88ee66fae9ebe8dd644695b26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3215b97ed373a02b59f77b133010135",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bf4a1cc381606ec4baa",
+    "url": "/static/css/main.c84d345f.chunk.css"
+  },
+  {
+    "revision": "bc28a601ba96ebc88ada",
+    "url": "/static/js/2.1a43eec9.chunk.js"
+  },
+  {
+    "revision": "5bf4a1cc381606ec4baa",
+    "url": "/static/js/main.001811a0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
