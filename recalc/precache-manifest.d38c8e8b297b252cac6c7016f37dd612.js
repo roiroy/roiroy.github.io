4d8b2de361497103908b2433afc91c093c46da76
itrend <itrend@gmail.com>
2019-05-16 23:57:36 +0300

    built from https://github.com/roiroy/recalc/commit/3e5e6cd0b26c22eb955cfcd0c7eca1efb78e47f6

---
+++ recalc/precache-manifest.d38c8e8b297b252cac6c7016f37dd612.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc988dc46bb88610837a4219e5c57bb2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e3f81f1eb0843cdd4e41",
+    "url": "./static/css/main.4d1dbc81.chunk.css"
+  },
+  {
+    "revision": "51f3a3c1432e093d69be",
+    "url": "./static/js/2.0beff45a.chunk.js"
+  },
+  {
+    "revision": "e3f81f1eb0843cdd4e41",
+    "url": "./static/js/main.b641b082.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- recalc/service-worker.js
+++ recalc/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/recalc/precache-manifest.388fc0bdf1416be6fd5edff785468950.js"
+  "./precache-manifest.d38c8e8b297b252cac6c7016f37dd612.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/recalc/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
